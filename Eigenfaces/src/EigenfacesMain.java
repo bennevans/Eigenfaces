5773082e618bf0673e16f24740d5c964f706f4d4
Ben Evans <otterdude97@gmail.com>
2015-05-26 22:36:23 -0700

    messed around; project still not working

--- Eigenfaces/src/EigenfacesMain.java
+++ Eigenfaces/src/EigenfacesMain.java
@@ -2,6 +2,8 @@
 
 import java.util.ArrayList;
 
+import org.opencv.contrib.Contrib;
+import org.opencv.contrib.FaceRecognizer;
 import org.opencv.core.*;
 
 public class EigenfacesMain {
@@ -10,9 +12,9 @@ public class EigenfacesMain {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 		
-		Mat m1 = Mat.eye(new Size(2, 2), CvType.CV_64FC1);
-		Mat m2 = Mat.eye(new Size(2, 2), CvType.CV_64FC1);
-		Mat m3 = Mat.eye(new Size(2, 2), CvType.CV_64FC1);
+		Mat m1 = Mat.eye(new Size(2, 2), EigenUtil.TYPE);
+		Mat m2 = Mat.eye(new Size(2, 2), EigenUtil.TYPE);
+		Mat m3 = Mat.eye(new Size(2, 2), EigenUtil.TYPE);
 		Core.multiply(m2, new Scalar(255), m2);
 		m3.put(0, 0, 50);
 		m3.put(1, 0, 32);
@@ -24,7 +26,16 @@ public class EigenfacesMain {
 		mats.add(m3);
 		Eigenfaces e = new Eigenfaces(mats);
 		System.out.println(e);
-		System.out.println(e.project(m3).dump());
+		System.out.println("normalized:\n"+EigenUtil.normalize(e.project(m3)).dump());
+
+		
+		for(Mat m : e.getEigenvectors()){
+			m = EigenUtil.normalize(m);
+			System.out.println("eigen vector: " + m.dump());
+		}
+		
+		
+		
 	}
 	
 }
